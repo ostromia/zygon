1272d25e4e2dfeef2b7d1789073636ba93cfd86b
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-04-24 21:04:47 +0100

    Mon 24 Apr 2023 09:04:47 PM BST

---
+++ _app/immutable/chunks/0.20d35520.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout.da46b06b.js";import{default as t}from"../entry/layout.svelte.e1a54c18.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/2.56d83df8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.a5ff9245.js";export{t as component};
