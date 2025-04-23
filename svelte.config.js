a2866a1d1872c1ccdb7d505e72e3ada35d8575f9
ostromia <66202981+ostromia@users.noreply.github.com>
2025-04-23 17:14:03 +0100

    chore: clean up config files

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import path from 'path';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
-
-    kit: {
-		adapter: adapter({
-		    pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-
-        paths: {
-            relative: false,
-            base: process.env.NODE_ENV == 'development' ? '' : '/zygon'
-        },
-
-        alias: {
-            $r: path.resolve('src/routes'),
-            $index: path.resolve('src/routes/(index)')
-        }
-    }
-
+	preprocess: vitePreprocess(),
+	kit: { adapter: adapter() }
 };
 
 export default config;
