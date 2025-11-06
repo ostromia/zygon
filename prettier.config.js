986db9a3294c833928a5ea81f5cf64be5409b14b
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2025-11-06 23:07:42 +0000

    chore: configure prettier

---
+++ prettier.config.js
@@ -0,0 +1,24 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    useTabs: false,
+    tabWidth: 4,
+    singleQuote: false,
+    trailingComma: "none",
+    printWidth: 100,
+
+    plugins: ["prettier-plugin-svelte"],
+
+    overrides: [
+        {
+            files: "*.svelte",
+            options: {
+                parser: "svelte"
+            }
+        }
+    ]
+};
+
+export default config;
