834206622af003efa2bb2a3f660ce68a88f2174f
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2025-11-07 00:18:31 +0000

    fix: set favicon through assets

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -1,21 +1,26 @@
-<script>
+<script lang="ts">
     import { VERSION } from "svelte/compiler";
+    import favicon from "$lib/assets/favicon.png";
 
     const title = "Zygon";
     const description = "Transpile GCSE & A-Level pseudocode into Python.";
-    const canonicalURL = "https://ostromia.github.io/zygon/";
+    const canonical = "https://ostromia.github.io/zygon/";
 </script>
 
 <svelte:head>
+    <!-- Global Metadata -->
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <meta name="generator" content="Svelte {VERSION}" />
-
-    <!-- Canonical URL -->
-    <link rel="canonical" href={canonicalURL} />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="generator" content={`SvelteKit ${VERSION}`} />
 
     <!-- Primary Meta Tags -->
     <title>{title}</title>
-    <meta name="title" content={title} />
     <meta name="description" content={description} />
+
+    <!-- Favicon -->
+    <link rel="image/png" href={favicon} />
+    <link rel="apple-touch-icon" sizes="180x180" href={favicon} />
+
+    <!-- Canonical URL -->
+    <link rel="canonical" href={canonical} />
 </svelte:head>
